4b3959d0f10ada86c394497ab2e6819e87d85e57
maxvanny2010 <mailtime2010@gmail.com>
2025-10-09 16:27:46 -0500

    chore(store): add store provider wrapper

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,6 @@
 import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from './store';
 
+/** hooks for store **/
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
